a99e357ec49717def72d09379f696a918f63b68c
Zamponi Marco <zamponi.marco3@gmail.com>
2021-08-21 16:52:18 +0200

    [UPDATE] Fixed A* algorithm

--- project.lua
+++ project.lua
@@ -7,7 +7,6 @@ grid = {
 
 meta_grid = {}
 
-
 male = {
   {1, 2},
   {3, 1}
@@ -19,12 +18,16 @@ female = {
   {2, 5}
 }
 
+function set_default (t, d)
+  local mt = {__index = function () return d end}
+  setmetatable(t, mt)
+end
+
 function init_meta_grid()
   for i, row in ipairs(grid) do
     table.insert(meta_grid, {})
     for k, _ in ipairs(row) do
       table.insert(meta_grid[i], {i, k})
-      ---meta_grid[i][k] = {i, k}
     end
   end
 end
@@ -48,17 +51,18 @@ end
 
 function debug_table(table) 
   for k,v in pairs(table) do
-      print(k, v)
+      io.write(k .. ":" .. v)
+      print()
   end
 end
 
 function lowest_point(open_points, point_map)
-  min = 10000000
+  min = 1/0
   to_return = nil
   index = 0
   for i,point in ipairs(open_points) do
-    if (point_map[point]["f"] < min) then
-      min = point_map[point]["f"]
+    if (f[point] < min) then
+      min = f[point]
       to_return = point
       index = i
     end
@@ -66,21 +70,25 @@ function lowest_point(open_points, point_map)
   return index, to_return
 end
 
+function heuristics(point, destination, grid)
+  return math.abs(grid[point[1]][point[2]] - grid[destination[1]][destination[2]])
+end
+
 function shortest_path(start, destination, grid) 
   _start = to_meta(start)
   _destination = to_meta(destination)
   closed_points = { }
   open_points = { _start }
-  point_map = { 
-    [_start] = {g = 0, h = 0, f = 100, parent = {}}
-  }
+  g = { [_start] = 0 }
+  set_default(g, 1/0)
+  f = { [_start] = heuristics(_start, _destination, grid) }
+  parent = {}
   
   while (#open_points > 0) do
-    i, point = lowest_point(open_points, point_map)
+    i, point = lowest_point(open_points, f)
     
     if (point == _destination) then
-      print("Arrived")
-      return "da fare"
+      return recreate_path(_destination, parent)
     end
     
     table.remove(open_points, i)
@@ -90,27 +98,30 @@ function shortest_path(start, destination, grid)
       _neighbour = to_meta(neighbour)
       if (not contains_point(closed_points, _neighbour)) then 
         
-        t_g = point_map[point]["g"] + distance_between(point, _neighbour, grid)
-
-        if (not contains_point(open_points, _neighbour)) then
-          table.insert(open_points, _neighbour)
-          better = true
-        elseif (t_g < point_map[_neighbour]["g"]) then
-          better = true
-        else
-          better = false
-        end
+        t_g = g[point] + distance_between(point, _neighbour, grid)
         
-        if (better) then
-          point_map[_neighbour] = {g = t_g, h = 0, f = t_g + 0, parent = point}
+        if (t_g < g[_neighbour]) then
+          g[_neighbour] = t_g
+          f[_neighbour] = t_g + heuristics(_neighbour, _destination, grid)
+          parent[_neighbour] = point
+          if (not contains_point(open_points, _neighbour)) then
+            table.insert(open_points, _neighbour)
+          end
         end
-        
       end
-      
     end
-    
   end
   return nil
+end
+
+function recreate_path(destination, parent)
+  current = destination
+  final = {}
+  while parent[current] ~= nil do
+    table.insert(final, 1, current)
+    current = parent[current]
+  end
+  return final
 end
 
 function distance_between(point, neighbour, grid)
@@ -132,13 +143,23 @@ function neighbour_points(point, grid)
   for _, offset in ipairs(offsets) do
     new_x = x + offset[1]
     new_y = y + offset[2]
-    if new_x > 0 and new_x < #grid[1] then
-      if new_y > 0 and new_y < #grid then
-        table.insert(res, {new_x, new_y})
+    if new_x > 0 and new_x <= #grid then
+      if new_y > 0 and new_y <= #grid[1] then
+        if grid[new_x][new_y] ~= 0 then
+          table.insert(res, {new_x, new_y})
+        end
       end
     end
   end
   return res
 end
 
-shortest_path({1,1}, {1,5}, grid)
+result = shortest_path({1,1}, {1,5}, grid)
+
+if result then
+  for _,v in pairs(result) do
+    print(v[1] .. " " .. v[2])
+  end
+else
+  print("No Solution")
+end
